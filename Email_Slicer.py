bf0e212bd22b49927d7b335427c3cc1c5b83a47e
Hardik Gupta <29226479+Hardikg13@users.noreply.github.com>
2022-09-27 00:08:34 +0530

    Add files via upload

---
+++ Email_Slicer.py
@@ -0,0 +1,18 @@
+def main():
+    print("Welcome to Email Slicer")
+    print(" ")
+
+    email_input = input("Enter your email address:")
+
+    (username,domain) = email_input.split("@")
+    (domain,extension) = domain.split(".")
+
+    print("username =",username)
+    print("domain =",domain)
+    print("extension =",extension)
+
+while True:
+    main()
+
+
+
